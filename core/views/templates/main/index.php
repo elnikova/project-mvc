2e6ebf25132514d6c15dcc11a6b059c9e49b60e1
Dariya Elnikova <elnikova.dariya@gmail.com>
2020-11-29 13:20:39 +0200

    dz

--- core/views/templates/main/index.php
+++ core/views/templates/main/index.php
@@ -1,6 +1,9 @@
 <h1 class="bg-primary"><?= $title ?></h1>
 
+<h2>Lorem, ipsum.</h2>
+
 <?php foreach($articles as $article): ?>
     <h2><?= $article['name'] ?></h2>
     <p><?= $article['content'] ?></p>
 <?php endforeach ?>
+
